646730c0d944a6d28923c07ea0e7cfa19c14e870
shyeokchoi <shyeokchoi@gmail.com>
2024-08-05 14:32:56 +0900

    main function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
+#include "include/bindings.h"
 #include <stdio.h>
+#include <vector>
 
-int main(int, char**){
-    printf("Hello, from cbindgen-example!\n");
+ffi::VecWrapper<const char*> get_strings()
+{
+    std::vector<const char*> strings { "Hello,", "from", "C++" };
+
+    return ffi::VecWrapper<const char*> {
+        .data = strings.data(),
+        .len = strings.size(),
+        .capacity = strings.capacity()
+    };
+}
+
+int main()
+{
+    ffi::test_hello();
+
+    auto strings = get_strings();
+    ffi::print_strings(&strings);
 }

8e9dc5e2cf16f33c6b02d4d9c6fa3bcfab1fc6e8
shyeokchoi <shyeokchoi@gmail.com>
2024-08-05 13:48:02 +0900

    print_strings function

--- rust-part/src/lib.rs
+++ rust-part/src/lib.rs
@@ -1,6 +1,28 @@
+use std::ffi::{c_char, CStr};
+
+use data_structure::vec_wrapper::VecWrapper;
+
 mod data_structure;
 
 #[no_mangle]
 pub fn test_hello() {
     println!("Hello, from Rust!")
+}
+
+#[no_mangle]
+pub fn print_strings(strings: &VecWrapper<*const c_char>) {
+    let string_vec: Vec<String> = strings
+        .to_vec()
+        .iter()
+        .map(|cchar| unsafe {
+            CStr::from_ptr(*cchar)
+                .to_str()
+                .expect("CStr to &str failed.")
+                .to_string()
+        })
+        .collect();
+
+    for string in string_vec {
+        println!("{string}")
+    }
 }
